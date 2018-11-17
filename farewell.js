46c3d8870eebf7d0f92b0e6bdef82f70256ce027
Thomas Charlton <tchrlton1@gmail.com>
2018-11-16 20:21:08 -0800

    created greeting and farewell modules

---
+++ farewell.js
@@ -0,0 +1,15 @@
+exports.farewellEnglish = function(){
+    console.log("Goodbye");
+}
+
+exports.farewellJapanese = function(){
+    console.log("Sayonara");
+}
+
+exports.farewellSpanish = function(){
+    console.log("Adios");
+}
+
+exports.farewellIcelandic = function(){
+    console.log("Bless");
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+const greetings = require('./greeting.js');
+const farewells = require('./farewell.js');
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+
+farewells.farewellEnglish();
+
+farewells.farewellJapanese();
+
+farewells.farewellSpanish();
+
+farewells.farewellIcelandic();
